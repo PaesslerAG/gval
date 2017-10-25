85a7c2c497668794d9bf87a886978a82eda598dc
JanErik Keller <janerik.keller@paessler.com>
2017-10-25 17:29:37 +0200

    added evalInt testcase

--- evaluable_test.go
+++ evaluable_test.go
@@ -1,6 +1,9 @@
 package gval
 
-import "testing"
+import (
+	"context"
+	"testing"
+)
 
 func TestEvaluable_IsConst(t *testing.T) {
 	p := Parser{}
@@ -24,6 +27,34 @@ func TestEvaluable_IsConst(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := tt.e.IsConst(); got != tt.want {
 				t.Errorf("Evaluable.IsConst() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestEvaluable_EvalInt(t *testing.T) {
+	tests := []struct {
+		name    string
+		e       Evaluable
+		want    int
+		wantErr bool
+	}{
+		{
+			"point",
+			constant("5.3"),
+			5,
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.e.EvalInt(context.Background(), nil)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Evaluable.EvalInt() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Evaluable.EvalInt() = %v, want %v", got, tt.want)
 			}
 		})
 	}
